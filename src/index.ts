659511e8c484935e24754fa478305557d5a9b8ae
Dw1GHTY <mih.mare@gmail.com>
2023-09-12 01:15:42 +0200

    Json-server i fetch
    sredina poslednjeg termina za RxJS

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-import { interval, map, take, range, filter, Subscription, takeUntil, Observable, Subject, fromEvent, sampleTime, debounceTime } from "rxjs";
+import {
+    interval,
+    map,
+    take,
+    range,
+    filter,
+    Subscription,
+    takeUntil,
+    Observable,
+    Subject,
+    fromEvent,
+    sampleTime,
+    debounceTime
+} from "rxjs";
+import { Movie } from "./movie";
 
+const URL = "http://localhost:3000/movies/";
+
+function getMovie(title: string): Promise<Movie> {
+    return fetch(URL + title)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Movie not found!");
+            } else {
+                return response.json();
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+getMovie("rocky");
+
+//stao si na 31:15
